09928df9a5581f7e01969b8f2328b477ebd86617
TomasUNNE <89489444+TomasUNNE@users.noreply.github.com>
2025-10-25 19:26:38 -0300

    Corrección al script del caso de estudio.

--- script/Caso_gimnasio.sql
+++ script/Caso_gimnasio.sql
@@ -12,69 +12,99 @@ CREATE TABLE Rol
   CONSTRAINT PK_rol PRIMARY KEY (id_rol)
 );
 
-CREATE TABLE Permiso
+CREATE TABLE Estado
 (
-  id_permiso INT NOT NULL,
-  nombre_menu VARCHAR(100) NOT NULL,
-  id_rol INT NOT NULL,
-  CONSTRAINT PK_permiso PRIMARY KEY (id_permiso),
-  CONSTRAINT FK_rol FOREIGN KEY (id_rol) REFERENCES Rol(id_rol)
+  id_estado INT NOT NULL,
+  descripcion VARCHAR(100) NOT NULL,
+  CONSTRAINT PK_Estado PRIMARY KEY (id_estado)
 );
 
-CREATE TABLE Usuario
+CREATE TABLE telefono
+(
+  id_telefono INT NOT NULL,
+  numero INT NOT NULL,
+  CONSTRAINT PK_telefono PRIMARY KEY (id_telefono)
+);
+
+CREATE TABLE Personal
 (
   id_usuario INT NOT NULL,
   nombre VARCHAR(100) NOT NULL,
   apellido VARCHAR(100) NOT NULL,
-  email VARCHAR(200) NOT NULL,
+  correo VARCHAR(200) NOT NULL,
   dni INT NOT NULL,
-  fecha_nac DATE NOT NULL,
-  sexo CHAR(1) NOT NULL,
   id_rol INT NOT NULL,
-  CONSTRAINT PK_usuario PRIMARY KEY (id_usuario),
-  CONSTRAINT FK_rol_usuario FOREIGN KEY (id_rol) REFERENCES Rol(id_rol)
-);
-
-CREATE TABLE Ejercicio
-(
-  id_ejercicio INT NOT NULL,
-  nombre_ejercicio VARCHAR(200) NOT NULL,
-  repeticiones INT NOT NULL,
-  tiempo TIME NOT NULL,
-  CONSTRAINT PK_ejercicio PRIMARY KEY (id_ejercicio)
-);
-
-CREATE TABLE Plan_Entrenamiento
-(
-  id_plan INT NOT NULL,
-  nombre_plan VARCHAR(200) NOT NULL,
-  fecha_inicio DATE NOT NULL,
-  fecha_fin DATE NOT NULL,
-  id_ejercicio INT NOT NULL,
-  CONSTRAINT PK_plan PRIMARY KEY (id_plan),
-  CONSTRAINT FK_ejercicio FOREIGN KEY (id_ejercicio) REFERENCES Ejercicio(id_ejercicio)
+  id_estado INT NOT NULL,
+  id_telefono INT NOT NULL,
+  CONSTRAINT PK_Usuario PRIMARY KEY (id_usuario),
+  CONSTRAINT FK_rol FOREIGN KEY (id_rol) REFERENCES Rol(id_rol),
+  CONSTRAINT FK_estado_personal FOREIGN KEY (id_estado) REFERENCES Estado(id_estado),
+  CONSTRAINT FK_telefono_personal FOREIGN KEY (id_telefono) REFERENCES telefono(id_telefono)
 );
 
 CREATE TABLE Membresia
 (
   id_membresia INT NOT NULL,
-  nombre_membresia VARCHAR(200) NOT NULL,
-  duracion INT NOT NULL,
-  costo FLOAT NOT NULL,
+  nombre VARCHAR(200) NOT NULL,
+  precio FLOAT NOT NULL,
   CONSTRAINT PK_membresia PRIMARY KEY (id_membresia)
 );
 
-CREATE TABLE Socio
+CREATE TABLE Alumno
 (
-  observaciones VARCHAR(300) NOT NULL,
-  id_socio INT NOT NULL,
-  id_usuario INT NOT NULL,
-  id_plan INT NOT NULL,
+  id_alumno INT NOT NULL,
+  nombre VARCHAR(100) NOT NULL,
+  apellido VARCHAR(100) NOT NULL,
+  dni INT NOT NULL,
+  correo VARCHAR(100) NOT NULL,
+  fecha_nacimiento DATE NOT NULL,
+  sexo CHAR(1) NOT NULL,
+  id_estado INT NOT NULL,
   id_membresia INT NOT NULL,
-  CONSTRAINT PK_socio PRIMARY KEY (id_socio),
-  CONSTRAINT FK_usuario FOREIGN KEY (id_usuario) REFERENCES Usuario(id_usuario),
-  CONSTRAINT FK_plan FOREIGN KEY (id_plan) REFERENCES Plan_Entrenamiento(id_plan),
-  CONSTRAINT FK_membresia FOREIGN KEY (id_membresia) REFERENCES Membresia(id_membresia)
+  id_telefono INT NOT NULL,
+  CONSTRAINT PK_alumno PRIMARY KEY (id_alumno),
+  CONSTRAINT FK_estado_alumno FOREIGN KEY (id_estado) REFERENCES Estado(id_estado),
+  CONSTRAINT FK_membresia FOREIGN KEY (id_membresia) REFERENCES Membresia(id_membresia),
+  CONSTRAINT FK_telefono_alumno FOREIGN KEY (id_telefono) REFERENCES telefono(id_telefono)
+);
+
+
+CREATE TABLE Ejercicio
+(
+  id_ejercicio INT NOT NULL,
+  nombre_ejercicio VARCHAR(200) NOT NULL,
+  musculo_objetivo VARCHAR(100) NOT NULL,
+  CONSTRAINT PK_ejercicio PRIMARY KEY (id_ejercicio)
+);
+
+CREATE TABLE categoria
+(
+  id_categoria INT NOT NULL,
+  descripcion VARCHAR(100) NOT NULL,
+  CONSTRAINT PK_categoria PRIMARY KEY (id_categoria)
+);
+
+CREATE TABLE Plan_
+(
+  id_plan INT NOT NULL,
+  nombre VARCHAR(100) NOT NULL,
+  fecha_creacion DATE NOT NULL,
+  id_usuario INT NOT NULL,
+  id_categoria INT NOT NULL,
+  CONSTRAINT PK_Plan PRIMARY KEY (id_plan),
+  CONSTRAINT FK_Usuario FOREIGN KEY (id_usuario) REFERENCES Personal(id_usuario),
+  CONSTRAINT FK_categoria FOREIGN KEY (id_categoria) REFERENCES categoria(id_categoria)
+);
+
+CREATE TABLE Plan_Ejercicio
+(
+  repeticiones INT NOT NULL,
+  series INT NOT NULL,
+  id_ejercicio INT NOT NULL,
+  id_plan INT NOT NULL,
+  CONSTRAINT PK_Plan_Ejercicio PRIMARY KEY (id_ejercicio, id_plan),
+  CONSTRAINT FK_ejercicio FOREIGN KEY (id_ejercicio) REFERENCES Ejercicio(id_ejercicio),
+  CONSTRAINT FK_Plan_ej FOREIGN KEY (id_plan) REFERENCES Plan_(id_plan)
 );
 
 CREATE TABLE Metodo_pago
@@ -84,26 +114,24 @@ CREATE TABLE Metodo_pago
   CONSTRAINT PK_metodo_pago PRIMARY KEY (id_metodo_pago)
 );
 
-CREATE TABLE Pago_detalle
+CREATE TABLE Pago
 (
   fecha_pago DATE NOT NULL,
   monto_total FLOAT NOT NULL,
   id_pago INT NOT NULL,
-  id_socio INT NOT NULL,
   id_metodo_pago INT NOT NULL,
-  id_membresia INT NOT NULL,
-  CONSTRAINT PK_pago PRIMARY KEY (id_pago),
-  CONSTRAINT FK_socio FOREIGN KEY (id_socio) REFERENCES Socio(id_socio),
+  id_alumno INT NOT NULL,
+  CONSTRAINT PK_Pago PRIMARY KEY (id_pago),
   CONSTRAINT FK_metodo_pago FOREIGN KEY (id_metodo_pago) REFERENCES Metodo_pago(id_metodo_pago),
-  CONSTRAINT FK_membresia_pago FOREIGN KEY (id_membresia) REFERENCES Membresia(id_membresia)
+  CONSTRAINT FK_alumno_pago FOREIGN KEY (id_alumno) REFERENCES Alumno(id_alumno)
 );
 
-CREATE TABLE telefono
+CREATE TABLE Alumno_Plan
 (
-  id_telefono INT NOT NULL,
-  nro_telefono INT NOT NULL,
-  contacto_emergencia INT NOT NULL,
-  id_usuario INT NOT NULL,
-  CONSTRAINT PK_telefono PRIMARY KEY (id_telefono),
-  CONSTRAINT FK_usuario_telefono FOREIGN KEY (id_usuario) REFERENCES Usuario(id_usuario)
+  fecha_asignacion INT NOT NULL,
+  id_plan INT NOT NULL,
+  id_alumno INT NOT NULL,
+  CONSTRAINT PK_plan_alumno PRIMARY KEY (id_plan, id_alumno),
+  CONSTRAINT FK_plan_al FOREIGN KEY (id_plan) REFERENCES Plan_(id_plan),
+  CONSTRAINT FK_alumno FOREIGN KEY (id_alumno) REFERENCES Alumno(id_alumno)
 );
