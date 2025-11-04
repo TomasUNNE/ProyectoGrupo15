16d7fa50a048439368f171723571a083f492b01c
TomasUNNE <89489444+TomasUNNE@users.noreply.github.com>
2025-11-04 18:05:46 -0300

    Se añadio el script de manejo de permisos.
    Script aun en progreso.

---
+++ script/Manejo de permisos a nivel de usuarios de base de datos/Script de Manejo de Permisos.sql
@@ -0,0 +1,53 @@
+----- Implementacion de Manejo de Permisos a nivel usuarios de bases de datos
+
+
+
+--Se crean los usuarios a nivel servidor, sin roles asignados.
+
+create login manuel with password='Password123';
+create login juan with password='Password123';
+
+
+
+--Se crea los usuarios con los login anteriores para una base de datos ESPECIFICA
+
+USE Caso_gimnasio; ----- <-Verificar en que base de datos crear la instancia de estos usuarios.
+GO
+
+CREATE USER manuel FOR LOGIN manuel;
+CREATE USER juan FOR LOGIN juan;
+
+--- Se asignan distintos permisos a los usuarios creados para esta base de datos (caso_camiones)
+
+-- Por ejemplo, manuel solo puede leer las tablas, es decir esta restringido a usar solo la sentencia SELECT
+EXEC sp_addrolemember 'db_datareader', 'manuel';
+go
+
+
+--Probamos si los permisos funcionan. 
+EXECUTE AS USER = 'manuel'; --EXECUTE permite presentarse como el usuario mencionado, para ello, asegurarse de estar en una estancia de base de datos.
+
+SELECT * FROM persona --Debe ver las tablas.
+
+INSERT INTO Rol (id_rol, descripcion) VALUES (6, 'Seguridad');  ---No deberia poder insertar.
+
+REVERT  ---Regresas al estado inicial, sin el user manuel activo.
+
+
+
+
+--- Para el usuario Juan, se le otorga permisos de escritura, es decir, puedo usar unicamente sentencias INSERT 
+EXEC sp_addrolemember 'db_datawriter', 'juan';
+
+
+--- Probamos...
+EXECUTE AS USER = 'juan';
+SELECT * FROM persona --No deberia poder ver las tablas.
+INSERT INTO Rol (id_rol, descripcion) VALUES (6, 'Seguridad'); ---Deberia poder insertar.
+
+REVERT --- Recuerda salir del usuario activo.
+
+
+-- Ahora tambien le otorgamos permisos de administrador a juan.
+EXEC sp_addrolemember 'db_ddladmin','juan';
+go
