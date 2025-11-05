2e72d53fdee8067088a291ed3d62cfb299e2984d
FiorellaADeMichielis <127346959+FiorellaADeMichielis@users.noreply.github.com>
2025-11-05 14:28:01 -0300

    Modificacion de comentarios en el SQL script

--- script/Optimizacion_con_indices.sql
+++ script/Optimizacion_con_indices.sql
@@ -1,26 +1,26 @@
 /*	PROYECTO DE ESTUDIO - CASO GIMNASIO
-	Tema: Optimización de consultas a través de índices en la tabla PERSONA
+	Tema: OptimizaciÃ³n de consultas a travÃ©s de Ã­ndices en la tabla PERSONA
 */
 
--- Usamos la base de datos de tu proyecto
+-- Usamos la base de datos del proyecto
 USE Caso_gimnasio; 
 
 ----------------------------------------------------------------------
--- 0) Limpieza y Preparación
+--Limpieza y PreparaciÃ³n
 ----------------------------------------------------------------------
 
 -- Limpiar tablas si existen (para el estudio comparativo)
 IF OBJECT_ID('Persona_Sin_Indice') IS NOT NULL DROP TABLE Persona_Sin_Indice;
 IF OBJECT_ID('Persona_Con_Indice') IS NOT NULL DROP TABLE Persona_Con_Indice;
 
--- Creacion de tabla PERSONA (Persona_Sin_Indice) -> Tabla sin índice en el campo de busqueda 'correo'
+-- Creacion de tabla PERSONA (Persona_Sin_Indice) -Tabla sin Ã­ndice en el campo de busqueda 'correo'
 CREATE TABLE Persona_Sin_Indice
 (
-    id_persona INT IDENTITY(1,1) NOT NULL,
-    nombre VARCHAR(100) NOT NULL,
-    apellido VARCHAR(100) NOT NULL,
-    correo VARCHAR(200) NOT NULL,-- Columna que se utilizará para la búsqueda
-    dni INT NOT NULL,
-    CONSTRAINT PK_Persona_NoIndex PRIMARY KEY (id_persona)
-    -- UQ_Correo y UQ_DNI se omitenpara facilitar la comparación de rendimiento en el PK
+Â  Â  id_persona INT IDENTITY(1,1) NOT NULL,
+Â  Â  nombre VARCHAR(100) NOT NULL,
+Â  Â  apellido VARCHAR(100) NOT NULL,
+Â  Â  correo VARCHAR(200) NOT NULL,-- Columna que se utilizarÃ¡ para la bÃºsqueda
+Â  Â  dni INT NOT NULL,
+Â  Â  CONSTRAINT PK_Persona_NoIndex PRIMARY KEY (id_persona)
+    -- UQ_Correo y UQ_DNI se omiten para facilitar la comparaciÃ³n de rendimiento en el PK
 );
