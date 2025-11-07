f1ad6a58d4ba6b2af4c274b905f0ef9eca752c9c
Daminn7 <132027665+Daminn7@users.noreply.github.com>
2025-11-06 21:04:24 -0300

    Carga necesaria inicial

---
+++ Investigación/manejo_de_transacciones_y_transacciones_anidadas.sql
@@ -0,0 +1,3 @@
+-- Inserción inicial necesaria (se trabaja sobre un lote de datos de 9999 aunque no es necesario).
+INSERT INTO Metodo_pago (id_metodo_pago, nombre) VALUES (1, 'Efectivo');
+INSERT INTO Membresia (id_membresia, nombre, precio) VALUES (1, 'Básica', 29.99);
