c78c61287d4dd1636cfb55ba8d3d77a546ee2212
Daminn7 <132027665+Daminn7@users.noreply.github.com>
2025-11-15 19:43:55 -0300

    Delete Investigación directory
    Eliminación de carpeta por traslado a otra ubicación. Necesario para el pull request.

--- Investigación/manejo_de_transacciones_y_transacciones_anidadas.sql
+++
@@ -1,3 +0,0 @@
--- Inserción inicial necesaria (se trabaja sobre un lote de datos de 9999 aunque no es necesario).
-INSERT INTO Metodo_pago (id_metodo_pago, nombre) VALUES (1, 'Efectivo');
-INSERT INTO Membresia (id_membresia, nombre, precio) VALUES (1, 'Básica', 29.99);
