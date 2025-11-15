786bd87321a97b90531c67242fda7906350620d6
Daminn7 <132027665+Daminn7@users.noreply.github.com>
2025-11-15 20:01:37 -0300

    Actualización manejo_de_transacciones_y_transacciones_anidadas.sql
    Incorporación del Ejemplo 2: Transacción Fallida

--- script/Investigación/manejo_de_transacciones_y_transacciones_anidadas.sql
+++ script/Investigación/manejo_de_transacciones_y_transacciones_anidadas.sql
@@ -42,3 +42,42 @@ BEGIN CATCH
     PRINT 'Error: ' + ERROR_MESSAGE();
 END CATCH;
 GO
+
+--Transacción Fallida
+GO
+PRINT '--- INICIANDO TRANSACCIÓN FALLIDA ---';
+BEGIN TRAN inscribir_alumno_fallo;
+BEGIN TRY;
+
+    -- 1. Insertar Telefono (Se ejecuta)
+    INSERT INTO telefono (numero) VALUES ('555-9876');
+    DECLARE @NuevoTelefonoID INT = SCOPE_IDENTITY();
+    PRINT 'Teléfono creado con ID: ' + STR(@NuevoTelefonoID);
+
+    -- 2. Insertar Persona (Aquí sucedera la violación de restricción)
+    INSERT INTO Persona (nombre, apellido, correo, dni, id_telefono, id_estado)
+    VALUES ('Ana', 'Gomez', 'ana.gomez@email.com', 12345678, @NuevoTelefonoID, 1); -- DNI REPETIDO
+
+    -- (El código de abajo NUNCA se ejecuta)
+    DECLARE @NuevoPersonaID INT = SCOPE_IDENTITY();
+    DECLARE @NuevoPagoID INT = 10001; 
+
+    INSERT INTO Pago (id_pago, fecha_pago, monto_total, id_metodo_pago) 
+    VALUES (@NuevoPagoID, GETDATE(), 5000.0, 1);
+
+    INSERT INTO Alumno (fecha_nacimiento, sexo, id_membresia, id_persona, id_pago) 
+    VALUES ('1995-10-20', 'F', 1, @NuevoPersonaID, @NuevoPagoID);
+
+    COMMIT TRAN inscribir_alumno_fallo;
+
+END TRY
+BEGIN CATCH
+    -- El CATCH se activa por la violación de DNI único
+    ROLLBACK TRAN inscribir_alumno_fallo;
+    PRINT '--- TRANSACCIÓN FALLIDA (ROLLBACK) ---';
+    PRINT 'Error: ' + ERROR_MESSAGE();
+
+    -- Verificación: El teléfono '555-9876' NO existirá en la BD
+    SELECT COUNT(*) AS 'telefono_ana_gomez_existe' FROM telefono WHERE numero = '555-9876'; -- (Devolverá 0)
+END CATCH;
+GO
